ffafc3b05507ab336fb14289a1649b2bf3d6ee36
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-09-07 11:55:58 +0530

    Params

--- UseParams/Program.cs
+++ UseParams/Program.cs
@@ -3,12 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace UseParams
+public class MyClass
 {
-    class Program
+    public static void UseParams(params int[] list)
     {
-        static void Main(string[] args)
+        for (int i = 0; i < list.Length; i++)
         {
+            Console.Write(list[i] + " ");
         }
+        Console.WriteLine();
+    }
+
+    public static void UseParams2(params object[] list)
+    {
+        for (int i = 0; i < list.Length; i++)
+        {
+            Console.Write(list[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        
+        UseParams(10, 20, 30, 40, 50);
+        UseParams2(1, 'a', "test");
+
+        
+        UseParams2();
+
+        
+        int[] myIntArray = { 5, 6, 7, 8, 9 };
+        UseParams(myIntArray);
+
+        object[] myObjArray = { 2, 'b', "test", "again" };
+        UseParams2(myObjArray);
+
+        
+        UseParams2(myIntArray);
     }
 }
+    
+    
